c58f6d1190b552e329a29754fd520b1fac061490
Pablo N. Cornehl <seinoxygen@live.com>
2012-01-30 20:12:14 -0300

    Updated cart class.

--- classes/Cart.php
+++ classes/Cart.php
@@ -32,21 +32,62 @@ class Cart {
         $ME = &get_instance();
         $ME->load->library('session');
         $this->session = $ME->session;
+        $this->items = $this->session->get('cart');
     }
 
+    /**
+     * Add a new item in the cart.
+     * 
+     * @param array $item 
+     */
     public function add($item){
         if(is_array($item)){
-            array_push($this->items, $item);
+            foreach($item as $subitem){
+                if(!is_array($subitem)){
+                    $item['uid'] = sha1(serialize($item['options']));
+                    array_push($this->items, $item);
+                }
+                else{
+                    $item['uid'] = sha1(serialize($item['options']));
+                    array_push($this->items, $subitem);
+                }
+            }
         }
         $this->session->set('cart', $this->items);
     }
 
+    /**
+     * Edit an item in the cart.
+     * 
+     * @param array $item 
+     */
     public function edit($item){
-
+        $i = 0;
+        $remove = false;
+        for ($i = 0; $i < sizeof($this->items); $i++){
+            if($this->items[$i]['uid'] == $item['uid']){
+                if($item['quantity'] == 0){
+                    $remove = true;
+                }
+                break;
+            }
+        }
+        // Remove the item from the cart if the quantity is zero.
+        if($remove === true){
+            unset($this->items[$i]);
+        }
+        else{
+            array_merge($this->items[$i], $item);
+        }
     }
 
-    public function remove(){
-
+    /**
+     * Remove the item from the cart.
+     * 
+     * @param type $uid 
+     */
+    public function remove($uid){
+        $this->edit(array('uid' => $uid, 'quantity' => 0));
     }
 
     /**
@@ -60,6 +101,19 @@ class Cart {
             foreach($this->items as $item){
                 $total += $item['quantity'] * $item['price'];
             }
+        }
+        return $total;
+    }
+    
+    /**
+     * Return the total items in the cart.
+     * 
+     * @return integer
+     */
+    public function get_item_count(){
+        $total = 0;
+        foreach($this->items as $item){
+            $total += $item['quantity'];
         }
         return $total;
     }
