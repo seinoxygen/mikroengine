25d953d110ee6d6c16acdb550e5b7c43adbfea3d
Pablo N. Cornehl <seinoxygen@live.com>
2012-02-23 20:54:43 -0300

    Added config file for mails class

---
+++ config/mail.php
@@ -0,0 +1,25 @@
+<?php
+
+// Newlines. Don't change unless its specified by server.
+$config['newline'] = "\r\n";
+
+// The charset used in the emails
+$config['charset'] = "utf-8";
+
+// General priority
+$config['priority'] = 3;
+
+// Server address
+$config['smtp_server'] = '';
+
+// Server port
+$config['smtp_port'] = 25;
+
+// Default timeout
+$config['smtp_timeout'] = 60;
+
+// Username to login into mail
+$config['smtp_username'] = '';
+
+// Password used to login
+$config['smtp_password'] = '';
