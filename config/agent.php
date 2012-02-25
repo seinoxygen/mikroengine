f9261a6805cd49908e63532de2f4f8c73ad8441d
Pablo N. Cornehl <seinoxygen@live.com>
2012-02-25 13:58:27 -0300

    Added user agent config file.

---
+++ config/agent.php
@@ -0,0 +1,100 @@
+<?php
+
+$config['platforms'] = array(
+    // Windows
+    'Windows NT 6.1' => 'Windows Seven',
+    'windows nt 6.0' => 'Windows Vista',
+    'windows nt 5.2' => 'Windows 2003',
+    'windows nt 5.0' => 'Windows 2000',
+    'windows nt 5.1' => 'Windows XP',
+    'windows nt 4.0' => 'Windows NT 4.0',
+    'winnt4.0' => 'Windows NT 4.0',
+    'winnt 4.0' => 'Windows NT',
+    'winnt' => 'Windows NT',
+    'windows 98' => 'Windows 98',
+    'win98' => 'Windows 98',
+    'windows 95' => 'Windows 95',
+    'win95' => 'Windows 95',
+    'windows' => 'Unknown Windows OS',
+    // Mac
+    'os x' => 'Mac OS X',
+    'ppc mac' => 'Power PC Mac',
+    'freebsd' => 'FreeBSD',
+    'ppc' => 'Macintosh',
+    // Linux
+    'linux' => 'Linux',
+    'debian' => 'Debian',
+    // Others
+    'sunos' => 'Sun Solaris',
+    'opensolaris' => 'OpenSolaris',
+    'beos' => 'BeOS',
+    'apachebench' => 'ApacheBench',
+    'aix' => 'AIX',
+    'irix' => 'Irix',
+    'osf' => 'DEC OSF',
+    'hp-ux' => 'HP-UX',
+    'netbsd' => 'NetBSD',
+    'bsdi' => 'BSDi',
+    'openbsd' => 'OpenBSD',
+    'gnu' => 'GNU/Linux',
+    'unix' => 'Unknown Unix OS',
+    // Mobiles
+    'android' => 'Android',
+    // Gaming Consoles
+    'psp' => 'PlayStation Portable',
+    'playstation 3' => 'PlayStation 3',
+    'bunjalloo' => 'Nintendo DS',
+    'wii' => 'Wii'
+);
+
+$config['browsers'] = array(
+    'flock' => 'Flock',
+    'chrome' => 'Chrome',
+    'opera' => 'Opera',
+    'msiw' => 'Internet Explorer',
+    'internet explorer' => 'Internet Explorer',
+    'shiira' => 'Shiira',
+    'firefox' => 'Firefox',
+    'chimera' => 'Chimera',
+    'phoenix' => 'Phoenix',
+    'firebird' => 'Firebird',
+    'camino' => 'Camino',
+    'netscape' => 'Netscape',
+    'omniweb' => 'OmniWeb',
+    'safari' => 'Safari',
+    'mozilla' => 'Mozilla',
+    'konqueror' => 'Konqueror',
+    'icab' => 'iCab',
+    'lynx' => 'Lynx',
+    'links' => 'Links',
+    'hotjava' => 'HotJava',
+    'amaya' => 'Amaya',
+    'ibrowse' => 'IBrowse'
+);
+
+$config['mobile'] = array(
+    'android' => 'Android',
+    'blackberry' => 'BlackBerry',
+    'blazer' => 'Blazer',
+    'bolt' => 'Bolt',
+    'doris' => 'Doris',
+    'dorothy' => 'Dorothy',
+    'fennec' => 'Fennec',
+    'gobrowser' => 'Go Browser',
+    'iemobile' => 'IE Mobile',
+    'iris' => 'Iris',
+    'maemo browser' => 'Maemo Browser',
+    'mib' => 'Motorola Internet Browser',
+    'minimo' => 'Minimo',
+    'mobilexplorer' => 'Mobile Explorer',
+    'netfront' => 'NetFront Browser',
+    'operamini' => 'Opera Mini',
+    'opera mini' => 'Opera Mini',
+    'operamobi' => 'Opera Mobi',
+    'opera mobi' => 'Opera Mobi',
+    'semc-browser' => 'SEMC-Browser',
+    'skyfire' => 'Skyfire',
+    'teashark' => 'TeaShark',
+    'teleca' => 'Teleca-Obigo',
+    'uzardweb' => 'uZard Web'
+);
