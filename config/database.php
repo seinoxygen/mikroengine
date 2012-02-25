831f7537d47c282ddcb6bd172b73059bfc0f5bb3
Pablo N. Cornehl <seinoxygen@live.com>
2012-02-24 22:19:42 -0300

    Added comments.

--- config/database.php
+++ config/database.php
@@ -1,9 +1,22 @@
 <?php
 
+// Enable the database
 $config['db_enable'] = false;
+
+// Define database engine
 $config['db_engine'] = 'mysql';
+
+// Point the database connection
 $config['db_server'] = 'localhost';
+
+// Database username
 $config['db_username'] = '';
+
+// Database password
 $config['db_password'] = '';
+
+// Database name
 $config['db_database'] = '';
+
+// Extra options
 $config['db_options'] = array();
