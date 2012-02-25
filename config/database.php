3beb636e2b45aa591d1f93e742360f6db6a12655
Pablo N. Cornehl <seinoxygen@live.com>
2012-02-24 21:34:51 -0300

    Removed configurations.

--- config/database.php
+++ config/database.php
@@ -3,7 +3,7 @@
 $config['db_enable'] = false;
 $config['db_engine'] = 'mysql';
 $config['db_server'] = 'localhost';
-$config['db_username'] = 'root';
-$config['db_password'] = 'sein';
-$config['db_database'] = 'mikroengine';
+$config['db_username'] = '';
+$config['db_password'] = '';
+$config['db_database'] = '';
 $config['db_options'] = array();
