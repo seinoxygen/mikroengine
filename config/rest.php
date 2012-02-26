9139fdce62b77f949275c129c460334a9824748f
Pablo N. Cornehl <seinoxygen@live.com>
2012-02-25 23:33:56 -0300

    Created rest config file.

---
+++ config/rest.php
@@ -0,0 +1,12 @@
+<?php
+
+$config['format'] = 'json';
+
+// The table name that will host all keys and limits.
+$config['limit_table'] = 'rest';
+
+// Column on the table.
+$config['table_key'] = 'api_key';
+
+// Key name usedn in posts and gets so fetch the api key.
+$config['key_name'] = 'key';
