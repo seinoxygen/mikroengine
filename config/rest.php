fcfc1435eb98c12c0a7bd5f47191ff66d58feaf0
Pablo N. Cornehl <seinoxygen@live.com>
2012-02-26 18:28:00 -0300

    Updated config file.

--- config/rest.php
+++ config/rest.php
@@ -1,12 +1,32 @@
 <?php
 
+// Default format response.
 $config['format'] = 'json';
 
 // The table name that will host all keys and limits.
-$config['limit_table'] = 'rest';
+$config['table'] = '';
 
-// Column on the table.
-$config['table_key'] = 'api_key';
+/*
+ * Column in table where is stored api key.
+ */
+$config['table_key'] = '';
 
-// Key name usedn in posts and gets so fetch the api key.
-$config['key_name'] = 'key';
+/*
+ * Column in table that will save the total calls by the api key.
+ * Must be integer.
+ */
+$config['table_limit'] = '';
+
+/**
+ * Column in table that will host the time of the last call.
+ * Must be integer.
+ */
+$config['table_time'] = '';
+
+/**
+ * Key name usedn in posts and gets so fetch the api key.
+ * 
+ * GET: http://www.domain.com/resource/method/key/e10adc3949ba59ab
+ * POST: $_POST['key']
+ */
+$config['key'] = 'key';
