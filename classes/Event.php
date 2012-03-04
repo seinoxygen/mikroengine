51acb905fd1cc5510a909dbb6524b0b4bd5c56fc
Pablo N. Cornehl <seinoxygen@live.com>
2012-03-04 13:03:07 -0300

    Added basic observer pattern.

--- classes/Event.php
+++ classes/Event.php
@@ -22,41 +22,56 @@
  */
 
 // ------------------------------------------------------------------------
-namespace Mikroengine\Core;
 
 class Event {
+   
+    private $object;
+    private $name = '';
+    private $ME;
+
+    public function __construct($object = null) {
+        if(is_null($object)){
+            return;
+        }
+        $this->name = get_class($object);
+        $this->object = $object;
+        $this->ME = &get_instance();
+    }
     
-    protected static $_events = array();
-    
-    public function __construct(){
+    public function __call($method, $args) {
         
-    }
-    
-    public static function register($name, $callback, $arguments = array()){
-        if(!isset(static::$_events[$name])){
-            static::$_events[$name] = array();
+        // Call event function before the real call.
+        $before = 'onBefore' .  ucfirst($method) . 'Event';
+        if (method_exists($this->ME, $before)) {
+            call_user_func_array(array($this->ME, $before), $args);
         }
-        array_push(static::$_events[$name], array('method' => $callback, 'args' => $arguments));
-    }
-    
-    public static function trigger($name, $data){
-        if(static::has_listeners($name)){
-            foreach (static::$_events[$name] as $callback) {
-                if(is_callable($callback['method'])){
-                    $arguments = !empty($callback['args']) ? $callback['args'] : array();
-                    call_user_func_array($callback['method'], $arguments);
-                }
-            }
+        
+        // Call the method in the plugin class.
+        if (method_exists($this->object, $method)) {
+            call_user_func_array(array($this->object, $method), $args);
+        }
+        
+        // Method was called so trigger the event.
+        $on = 'on' .  ucfirst($method) . 'Event';
+        if (method_exists($this->ME, $on)) {
+            call_user_func_array(array($this->ME, $on), $args);
+        }
+        
+        // Finally call the last event if it exists.
+        $after = 'onAfter' . ucfirst($method) . 'Event';
+        if (method_exists($this->ME, $after)) {
+            call_user_func_array(array($this->ME, $after), $args);
         }
     }
-    
-    public static function has_listeners($name){
-        return (count(static::$_events[$name])>0) ? true : false;
-    }
+} 
+
+// END Event Class
+
+class Listener extends Event {
     
 }
 
-// END Event Class
+// END Listener Class
 
 /* End of file Event.php */
 /* Location: Event.php */
