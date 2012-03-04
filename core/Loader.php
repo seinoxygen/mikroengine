d397751be8e9b806fabc3805c557e949ced6d40d
Pablo N. Cornehl <seinoxygen@live.com>
2012-03-04 13:07:29 -0300

    Added plugin loader function.

--- core/Loader.php
+++ core/Loader.php
@@ -2,7 +2,7 @@
 
 /**
  * @package		Mikroengine
- * @author		Mikrobytes Dev Team
+ * @author		Keovi Dev Team
  * @copyright           Copyright (c) 2011 - 2011, Mikrobytes.
  * @license
  * @link		http://www.mikroengine.com
@@ -18,21 +18,39 @@
  * @package		Mikroengine
  * @subpackage          Libraries
  * @category            Loader
- * @author		Mikrobytes Dev Team
+ * @author		Keovi Dev Team
  */
 
 // ------------------------------------------------------------------------
 
 class Loader {
 
-    public function __construct() {
-    }
-
+    /**
+     * Load a class.
+     *
+     * @param string $file
+     * @param string $name
+     * @param array $args 
+     */
     public function library($file, $name = '', $args = '') {
         if(!include_once("classes/".ucfirst($file).".php")) throw new Exception('Library file not found.');
         $name = (empty($name)) ? strtolower($file) : $name;
         $ME = &get_instance();
         $ME->{$name} = new $file($args);        
+    }
+    
+    /**
+     * Load plugin class and asign it to a listener.
+     * 
+     * @param string $file
+     * @param string $name
+     * @param array $args
+     */
+    public function plugin($file, $name = '', $args = ''){
+        if(!include_once("plugins/".ucfirst($file).".php")) throw new Exception('Plugin file not found.');
+        $name = (empty($name)) ? strtolower($file) : $name;
+        $ME = &get_instance();
+        $ME->{$name} = new Listener(new $file($args));
     }
     
     /**
@@ -48,14 +66,24 @@ class Loader {
         $ME->{$name} = new $file();
     }
         
+    /**
+     * Load a view.
+     * 
+     * @param string $file
+     * @param array $vars
+     * @param boolean $return
+     * @return string 
+     */
     public function view($file, $vars = array(), $return = false) {
         $ME = &get_instance();
-        
         if($return === true){
             return $ME->output->load($file, $vars, true);
         }
-        
         $ME->output->load($file, $vars);
     }
-  
 }
+
+// END Loader Class
+
+/* End of file Loader.php */
+/* Location: Loader.php */
